5510c9d7b4bf4c13b594bd8093dce2446e3e8199
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-25 11:18:18 -0300

    refactor: add secure service content for car brand service

--- src/app/services/car-brand.service.ts
+++ src/app/services/car-brand.service.ts
@@ -1,9 +1,32 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CarBrand } from '../models/CarBrand';
+import { Observable } from 'rxjs';
+
+interface CarResponse {
+  Makes: Array<any>;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarBrandService {
+  private CARS_API =
+    'http://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  private brands(brands): CarBrand[] {
+    return brands.map((brand) => ({
+      code: brand.make_id,
+      name: brand.make_display,
+    }));
+  }
+
+  public getMarcas(): Observable<CarBrand[]> {
+    return this.http
+      .jsonp(this.CARS_API, 'callback')
+      .pipe(map((res: CarResponse) => this.brands(res.Makes)));
+  }
 }
