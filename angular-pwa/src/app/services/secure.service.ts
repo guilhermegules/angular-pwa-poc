b8b43ec2a22daf6a2a5900e8575e69b12d40bff4
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-26 10:37:23 -0300

    feat: adding dexie lib and store info in indexedDB

--- angular-pwa/src/app/services/secure.service.ts
+++ angular-pwa/src/app/services/secure.service.ts
@@ -3,32 +3,65 @@ import { Observable } from 'rxjs';
 import { Secure } from 'src/app/models/Secure';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import Dexie from 'dexie';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SecureService {
   private API = 'http://localhost:9000';
+  private db: Dexie;
+  private table: Dexie.Table<Secure, any> = null;
 
   constructor(
     private http: HttpClient,
     private onlineOfflineService: OnlineOfflineService
   ) {
     this.connectionStatusListener();
+    this.initDb();
   }
 
-  public saveApi(secure: Secure) {
+  private initDb() {
+    this.db = new Dexie('db-secures');
+    this.db.version(1).stores({
+      secure: 'carBoard',
+    });
+    this.table = this.db.table('secure');
+  }
+
+  public saveApi(secure: Secure): void {
     this.http.post(`${this.API}/api/seguros`, secure).subscribe(
       () => alert('Seguro foi cadastrado com sucesso'),
       (err) => console.log('Erro ao cadastrar seguro')
     );
   }
 
-  public registry(secure: Secure) {
+  private async saveIndexedDb(secure: Secure): Promise<any> {
+    try {
+      await this.table.add(secure);
+      const allSecures: Secure[] = await this.table.toArray();
+      console.log('Seguro foi salvo no indexDB', allSecures);
+    } catch (error) {
+      console.log('Erro ao salvar', error);
+    }
+  }
+
+  private async sendIndexedDb() {
+    const allSecures: Secure[] = await this.table.toArray();
+    for (const secure of allSecures) {
+      this.saveApi(secure);
+      await this.table.delete(secure.carBoard);
+      console.log(
+        `Seguro do carro com a placa ${secure.carBoard} foi excluido com sucesso`
+      );
+    }
+  }
+
+  public registry(secure: Secure): void {
     if (this.onlineOfflineService.isOnline) {
       this.saveApi(secure);
     } else {
-      console.log('salvar seguro no banco local');
+      this.saveIndexedDb(secure);
     }
   }
 
@@ -39,7 +72,7 @@ export class SecureService {
   private connectionStatusListener() {
     this.onlineOfflineService.conectionStatus.subscribe((online) => {
       if (online) {
-        console.log('Enviando os dados do meu banco local para API');
+        this.sendIndexedDb();
       } else {
         console.log('Estou offline');
       }
